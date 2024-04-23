ad1447a10c45a124d63390ae6160e201598d9485
Bryson-V <97585647+Bryson-V@users.noreply.github.com>
2024-04-23 16:42:40 -0700

    Update main.java

--- 006_Singly_Linked_Lists/base_code/main.java
+++ 006_Singly_Linked_Lists/base_code/main.java
@@ -5,6 +5,18 @@ import java.lang.*;
 
 class main {
 	public static void main(String args[]) {
-		
+	SinglyLinkedList n = new SinglyLinkedList();
+        for(int i=1; i<7; i++){
+            int temp = ((int)(Math.random()*253));
+            n.insert(i, i);
+        }
+        //testing cases
+        // n.swap(1, 2);
+
+        // n.remove(1);
+        // n.insert(3,2);
+        n.printList();
+        n.reverse();
+        n.printList();
 	}
 }
